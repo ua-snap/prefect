6cd5633f77ed0b8d7640d5f744817a91f7bee1ee
Bob Torgerson <rltorgerson@alaska.edu>
2024-01-19 17:06:46 -0900

    Prefect script to generate Slurm jobs on Chinook to create an indicator with given models and scenarios. Currently, configured for rx1day by default.

---
+++ indicators/indicator_functions.py
@@ -0,0 +1,87 @@
+from prefect import task
+import paramiko
+
+
+@task
+def check_for_nfs_mount(ssh, nfs_directory="/import/beegfs"):
+    stdin, stdout, stderr = ssh.exec_command(f"df -h | grep {nfs_directory}")
+
+    nfs_mounted = bool(stdout.read())
+
+    if not nfs_mounted:
+        raise Exception(f"NFS directory '{nfs_directory}' is not mounted")
+
+
+@task
+def clone_github_repository(ssh, branch, destination_directory):
+    target_directory = f"{destination_directory}/cmip6-utils"
+    stdin, stdout, stderr = ssh.exec_command(
+        f"if [ -d '{target_directory}' ]; then echo 'true'; else echo 'false'; fi"
+    )
+
+    directory_exists = stdout.read().decode("utf-8").strip() == "true"
+
+    if directory_exists:
+        # Directory exists, check the current branch
+        get_current_branch_command = (
+            f"cd {target_directory} && git branch --show-current"
+        )
+        stdin, stdout, stderr = ssh.exec_command(get_current_branch_command)
+        current_branch = stdout.read().decode("utf-8").strip()
+
+        if current_branch != branch:
+            print(f"Change repository branch to branch {branch}...")
+            # If the current branch is different from the desired branch, switch to the correct branch
+            switch_branch_command = f"cd {target_directory} && git checkout {branch}"
+            stdin, stdout, stderr = ssh.exec_command(switch_branch_command)
+
+        print(f"Pulling the GitHub repository on branch {branch}...")
+
+        # Run the Git pull command to pull the repository
+        git_pull_command = f"cd {target_directory} && git pull origin {branch}"
+        stdin, stdout, stderr = ssh.exec_command(git_pull_command)
+
+        # Wait for the Git command to finish and get the exit status
+        exit_status = stdout.channel.recv_exit_status()
+
+        # Check the exit status for errors
+        if exit_status != 0:
+            raise Exception(
+                f"Error cloning the GitHub repository. Exit status: {exit_status}"
+            )
+    else:
+        print(f"Cloning the GitHub repository on branch {branch}...")
+        # Run the Git clone command to clone the repository
+        git_command = f"cd {destination_directory} && git clone -b {branch} https://github.com/ua-snap/cmip6-utils.git"
+        stdin, stdout, stderr = ssh.exec_command(git_command)
+
+        # Wait for the Git command to finish and get the exit status
+        exit_status = stdout.channel.recv_exit_status()
+
+        # Check the exit status for errors
+        if exit_status != 0:
+            error_output = stderr.read().decode("utf-8")
+            raise Exception(
+                f"Error cloning the GitHub repository. Error: {error_output}"
+            )
+
+
+@task
+def create_and_run_slurm_script(
+    ssh, indicators, models, scenarios, slurm_script, input_dir, output_dir
+):
+    stdin, stdout, stderr = ssh.exec_command(
+        f"source ~/.bashrc && export PATH=$PATH:/opt/slurm-22.05.4/bin:/opt/slurm-22.05.4/sbin && python {slurm_script} --indicators '{indicators}' --models '{models}' --scenarios '{scenarios}' --input_dir '{input_dir}' --out_dir '{output_dir}'"
+    )
+
+    # Wait for the command to finish and get the exit status
+    exit_status = stdout.channel.recv_exit_status()
+
+    # Check the exit status for errors
+    if exit_status != 0:
+        error_output = stderr.read().decode("utf-8")
+        raise Exception(
+            f"Error creating or running Slurm scripts. Error: {error_output}"
+        )
+
+    print("Slurm scripts created and run successfully")
