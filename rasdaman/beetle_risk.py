97000a14922ec3ef67d7bcece92d89d3e8482735
Bob Torgerson <rltorgerson@alaska.edu>
2024-02-23 22:00:46 -0900

    Updates all Rasdaman coverage flows to run locally.

--- rasdaman/beetle_risk.py
+++ rasdaman/beetle_risk.py
@@ -1,16 +1,9 @@
 from prefect import flow
-import paramiko
 import ingest_tasks
-
-# Define your SSH parameters
-ssh_host = "datacubes.earthmaps.io"
-ssh_port = 22
 
 
 @flow(log_prints=True)
 def beetle_risk(
-    ssh_username,
-    ssh_private_key_path,
     branch_name,
     working_directory,
     ingest_directory,
@@ -18,30 +11,15 @@ def beetle_risk(
     destination_directory,
     unzipped_directory,
 ):
-    # Create an SSH client
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ingest_tasks.clone_github_repository(branch_name, working_directory)
 
-    try:
-        # Load the private key for key-based authentication
-        private_key = paramiko.RSAKey(filename=ssh_private_key_path)
+    ingest_tasks.check_for_nfs_mount()
 
-        # Connect to the SSH server using key-based authentication
-        ssh.connect(ssh_host, ssh_port, ssh_username, pkey=private_key)
+    ingest_tasks.copy_data_from_nfs_mount(source_directory, destination_directory)
 
-        ingest_tasks.clone_github_repository(ssh, branch_name, working_directory)
+    ingest_tasks.unzip_files(destination_directory, unzipped_directory)
 
-        ingest_tasks.check_for_nfs_mount(ssh)
-
-        ingest_tasks.copy_data_from_nfs_mount(
-            ssh, source_directory, destination_directory
-        )
-
-        ingest_tasks.unzip_files(ssh, destination_directory, unzipped_directory)
-
-        ingest_tasks.run_ingest(ssh, ingest_directory)
-    finally:
-        ssh.close()
+    ingest_tasks.run_ingest(ingest_directory)
 
 
 if __name__ == "__main__":
@@ -49,13 +27,11 @@ if __name__ == "__main__":
         name="beetle_risk",
         tags=["beetle_risk"],
         parameters={
-            "ssh_username": "rltorgerson",
-            "ssh_private_key_path": "/Users/rltorgerson/.ssh/id_rsa",
             "branch_name": "main",
-            "working_directory": "/opt/rasdaman/user_data/rltorgerson/",
-            "ingest_directory": "/opt/rasdaman/user_data/rltorgerson/rasdaman-ingest/beetles/",
+            "working_directory": "/opt/rasdaman/user_data/snapdata/",
+            "ingest_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/beetles/",
             "source_directory": "/CKAN_Data/Base/Other/Spruce_Beetle_Risk/",
-            "destination_directory": "/opt/rasdaman/user_data/rltorgerson/rasdaman-ingest/beetles/risk_class/",
-            "unzipped_directory": "/opt/rasdaman/user_data/rltorgerson/rasdaman-ingest/beetles/risk_class/risk_class/",
+            "destination_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/beetles/risk_class/",
+            "unzipped_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/beetles/risk_class/risk_class/",
         },
     )
--- rasdaman/degree_days_below_zero.py
+++ rasdaman/degree_days_below_zero.py
@@ -1,44 +1,23 @@
 from prefect import flow
-import paramiko
 import ingest_tasks
-
-# Define your SSH parameters
-ssh_host = "datacubes.earthmaps.io"
-ssh_port = 22
 
 
 @flow(log_prints=True)
 def degree_days_below_zero(
-    ssh_username,
-    ssh_private_key_path,
     branch_name,
     working_directory,
     ingest_directory,
     source_directory,
     destination_directory,
 ):
-    # Create an SSH client
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    try:
-        # Load the private key for key-based authentication
-        private_key = paramiko.RSAKey(filename=ssh_private_key_path)
+    ingest_tasks.clone_github_repository(branch_name, working_directory)
 
-        # Connect to the SSH server using key-based authentication
-        ssh.connect(ssh_host, ssh_port, ssh_username, pkey=private_key)
+    ingest_tasks.check_for_nfs_mount("/workspace/Shared")
 
-        ingest_tasks.clone_github_repository(ssh, branch_name, working_directory)
+    ingest_tasks.copy_data_from_nfs_mount(source_directory, destination_directory)
 
-        ingest_tasks.check_for_nfs_mount(ssh, "/workspace/Shared")
-
-        ingest_tasks.copy_data_from_nfs_mount(
-            ssh, source_directory, destination_directory
-        )
-
-        ingest_tasks.run_ingest(ssh, ingest_directory, "hook_ingest.json")
-    finally:
-        ssh.close()
+    ingest_tasks.run_ingest(ingest_directory, "hook_ingest.json")
 
 
 if __name__ == "__main__":
@@ -46,12 +25,10 @@ if __name__ == "__main__":
         name="degree_days_below_zero",
         tags=["degree_days_below_zero"],
         parameters={
-            "ssh_username": "rltorgerson",
-            "ssh_private_key_path": "/Users/rltorgerson/.ssh/id_rsa",
             "branch_name": "main",
-            "working_directory": "/opt/rasdaman/user_data/rltorgerson/",
-            "ingest_directory": "/opt/rasdaman/user_data/rltorgerson/rasdaman-ingest/arctic_eds/degree_days/degree_days_below_zero/",
+            "working_directory": "/opt/rasdaman/user_data/snapdata/",
+            "ingest_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/arctic_eds/degree_days/degree_days_below_zero/",
             "source_directory": "/workspace/Shared/Tech_Projects/Arctic_EDS/project_data/rasdaman_datasets/degree_days_below_zero/",
-            "destination_directory": "/opt/rasdaman/user_data/rltorgerson/rasdaman-ingest/arctic_eds/degree_days/degree_days_below_zero/geotiffs/",
+            "destination_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/arctic_eds/degree_days/degree_days_below_zero/geotiffs/",
         },
     )
