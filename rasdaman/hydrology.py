6d56f53f35ad0f3a94d52404a873e8af987e5807
Bob Torgerson <rltorgerson@alaska.edu>
2024-03-13 13:18:45 -0800

    Adds hydrology Rasdaman coverage.

---
+++ rasdaman/hydrology.py
@@ -0,0 +1,36 @@
+from prefect import flow
+import ingest_tasks
+
+
+@flow(log_prints=True)
+def hydrology(
+    branch_name,
+    working_directory,
+    ingest_directory,
+    source_directory,
+    destination_directory,
+):
+
+    ingest_tasks.clone_github_repository(branch_name, working_directory)
+
+    ingest_tasks.check_for_nfs_mount("/workspace/Shared")
+
+    ingest_tasks.copy_data_from_nfs_mount(source_directory, destination_directory)
+
+    ingest_tasks.merge_data(ingest_directory)
+
+    ingest_tasks.run_ingest(ingest_directory)
+
+
+if __name__ == "__main__":
+    hydrology.serve(
+        name="hydrology",
+        tags=["hydrology"],
+        parameters={
+            "branch_name": "main",
+            "working_directory": "/opt/rasdaman/user_data/snapdata/",
+            "ingest_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/ardac/hydrology/",
+            "source_directory": "/workspace/Shared/Tech_Projects/Arctic_EDS/project_data/rasdaman_datasets/hydrology/",
+            "destination_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/ardac/hydrology/geotiffs/",
+        },
+    )
