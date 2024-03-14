45adfe8d00c8d56c91bdf1df3600f0ee8fdbec50
Josh Paul <99696041+Joshdpaul@users.noreply.github.com>
2024-03-14 15:40:36 -0800

    Add variable choice (#26)
    * add vars argument, pass to run_generate_batch_files() task
    
    * add luts.py to validate variable choices before starting the flow
    
    * add function to assert vars input is valid
    
    * expand luts.py to include land/sea/global vars, and add functionality to create the vars string from "all", "land", "sea", or "global"
    
    * black formatting

---
+++ regridding/luts.py
@@ -0,0 +1,61 @@
+"""Lookup tables for the regridding Prefect flow."""
+
+# use this list to validate variables
+# these were copied from the transfers/config.py in the cmip6-utils repo and include the WRF variables
+
+all_vars = [
+    "clt",
+    "evspsbl",
+    "hfls",
+    "hfss",
+    "hus",
+    "huss",
+    "mrro",
+    "mrsol",
+    "mrsos",
+    "orog",
+    "pr",
+    "prsn",
+    "ps",
+    "psl",
+    "rlds",
+    "rls",
+    "rsds",
+    "rss",
+    "sfcWind",
+    "sfcWindmax",
+    "sftlf",
+    "sftof",
+    "siconc",
+    "sithick",
+    "snd",
+    "snw",
+    "ta",
+    "tas",
+    "tasmax",
+    "tasmin",
+    "tos",
+    "ts",
+    "tsl",
+    "ua",
+    "uas",
+    "va",
+    "vas",
+    "zg",
+]
+
+land_vars = [
+    "mrro",
+    "mrsol",
+    "mrsos",
+    "snd",
+    "snw",
+]
+
+sea_vars = [
+    "tos",
+    "siconc",
+    "sithick",
+]
+
+global_vars = [i for i in all_vars if i not in land_vars + sea_vars]
