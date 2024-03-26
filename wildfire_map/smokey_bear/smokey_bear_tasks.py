d58ee3fec765e24fe47727b9948b5639afde321e
Bob Torgerson <rltorgerson@alaska.edu>
2024-03-25 16:07:49 -0800

    Adds the various Alaska Wildfire map layers to an automated Prefect script (#30)

---
+++ wildfire_map/smokey_bear/smokey_bear_tasks.py
@@ -0,0 +1,139 @@
+import os
+from prefect import task
+import subprocess
+
+
+@task
+def check_for_admin_pass(target_directory, admin_password):
+    file_path = f"{target_directory}/.adminpass"
+    admin_pass = f'export admin_pass="{admin_password}"\n'
+
+    if not os.path.exists(file_path):
+        # Only gets here if the file doesn't exist or the password is wrong
+        with open(file_path, "w") as file:
+            file.write(admin_pass)
+            return True
+    # Read the content of the local file
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    # Does the admin password match the supplied admin_password variable?
+    if admin_pass in content:
+        return True
+    else:
+        with open(file_path, "w") as file:
+            file.write(admin_pass)
+            return True
+
+
+@task(name="Install Smokey Bear Conda Environment")
+def install_conda_environment(conda_env_name, conda_env_file, local_install=False):
+    """
+    Task to check for a Python Conda environment and install it if it doesn't exist.
+    It also checks for Miniconda installation and installs Miniconda if it doesn't exist.
+    """
+
+    if local_install:
+        # Check if the Miniconda directory exists
+        miniconda_found = subprocess.run(
+            "test -d $HOME/miniconda3 && echo 1 || echo 0",
+            shell=True,
+            capture_output=True,
+            text=True,
+        ).stdout.strip()
+
+        miniconda_installed = bool(int(miniconda_found))
+
+        if not miniconda_installed:
+            print("Miniconda directory not found. Installing Miniconda...")
+            # Download and install Miniconda
+            subprocess.run(
+                "wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh && bash miniconda.sh -b -p $HOME/miniconda3",
+                shell=True,
+                check=True,
+            )
+            print("Miniconda installed successfully")
+
+        # Check if the Conda environment already exists
+        conda_env_exists = (
+            subprocess.run(
+                f"source $HOME/miniconda3/bin/activate && $HOME/miniconda3/bin/conda env list | grep {conda_env_name}",
+                shell=True,
+            ).returncode
+            == 0
+        )
+
+        if not conda_env_exists:
+            print(f"Conda environment '{conda_env_name}' does not exist. Installing...")
+
+            # Install the Conda environment from the environment file
+            subprocess.run(
+                f"source $HOME/miniconda3/bin/activate && $HOME/miniconda3/bin/conda env create -n {conda_env_name} -f {conda_env_file}",
+                shell=True,
+                check=True,
+            )
+            print(f"Conda environment '{conda_env_name}' installed successfully")
+        else:
+            print(f"Conda environment '{conda_env_name}' already exists.")
+    else:
+        # Check if the Miniconda directory exists
+        miniconda_found = subprocess.run(
+            "test -d /opt/miniconda3 && echo 1 || echo 0",
+            shell=True,
+            capture_output=True,
+            text=True,
+        ).stdout.strip()
+
+        miniconda_installed = bool(int(miniconda_found))
+
+        if not miniconda_installed:
+            print("/opt/miniconda3 directory not found.")
+            exit(1)
+
+        # Check if the Conda environment already exists
+        conda_env_exists = (
+            subprocess.run(
+                f"source /opt/miniconda3/bin/activate && /opt/miniconda3/bin/conda env list | grep {conda_env_name}",
+                shell=True,
+            ).returncode
+            == 0
+        )
+
+        if not conda_env_exists:
+            print(f"Conda environment '{conda_env_name}' does not exist. Installing...")
+
+            # Install the Conda environment from the environment file
+            subprocess.run(
+                f"source /opt/miniconda3/bin/activate && /opt/miniconda3/bin/conda env create -n {conda_env_name} -f {conda_env_file}",
+                shell=True,
+                check=True,
+            )
+            print(f"Conda environment '{conda_env_name}' installed successfully")
+        else:
+            print(f"Conda environment '{conda_env_name}' already exists.")
+
+
+@task(name="Execute Smokey Bear Local Script")
+def execute_local_script(script_path):
+    # Execute the script on the local machine
+    process = subprocess.Popen(
+        f"{script_path}",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout, stderr = process.communicate()
+    exit_code = process.returncode
+
+    output = stdout.decode("utf-8")
+    errors = stderr.decode("utf-8")
+
+    if exit_code == 0:
+        print(f"Processing output: {errors}")
+        print(f"Final output of the script {script_path}: {output}")
+        print(f"Script {script_path} executed successfully.")
+    else:
+        print(f"Error occurred while executing the script {script_path}.")
+        print(f"Error output: {errors}")
+
+    return exit_code, output, errors
---
+++ wildfire_map/smokey_bear/update_snow_cover.sh
@@ -0,0 +1,39 @@
+# Downloads data from upstream source,
+# Updates layer in GeoServer,
+# Rebuilds TileCache layer.
+set -x
+
+# Create a file in your $HOME directory containing an export
+# of the admin password.
+source ~/.adminpass
+
+# Activates the smokeybear Conda environment with GDAL installed.
+source /opt/miniconda3/bin/activate
+conda activate /home/snapdata/.conda/envs/smokeybear
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	ymd=$( date -d "yesterday" '+%Y%m%d' )
+	year=$( date -d "yesterday" '+%Y' )
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    ymd=$( date -v-1d +%Y%m%d )
+    year=$( date -v-1d +%Y )
+else
+	echo "OS unknown?"
+	exit 1
+fi
+
+script_dir=$(dirname "$(readlink -f "$0")")
+GEOSERVER_HOME=/usr/share/geoserver
+
+fname="NIC.IMS_v3_${ymd}_1km.tif"
+tname="snow_cover_3338.tif"
+wget -nc --content-disposition -O "/tmp/$fname.gz" "https://usicecenter.gov/File/DownloadProduct?products=%2Fims%2Fims_v3%2Fimstif%2F1km%2F${year}&fName=${fname}.gz"
+gunzip -f /tmp/$fname.gz
+gdal_translate -projwin -175.0 50.0 -80.0 55.0 -projwin_srs EPSG:4326 /tmp/$fname /tmp/snow_cover_crop.tif
+akcoast="$script_dir/shapefiles/Alaska_Coast_Simplified_POLYGON.shp"
+gdalwarp -crop_to_cutline -cutline ${akcoast} -overwrite -t_srs EPSG:3338 /tmp/snow_cover_crop.tif /tmp/snow_cover_warp.tif
+gdal_translate -projwin 173.2 77.0 -118.0 46.0 -projwin_srs EPSG:4326 /tmp/snow_cover_warp.tif /tmp/$tname
+mv /tmp/$tname $GEOSERVER_HOME/data_dir/data/alaska_wildfires/
+
+# Reseeds tile cache layer
+curl -v -u admin:${admin_pass} -XPOST -H "Content-type: text/xml" -d '<seedRequest><name>alaska_wildfires:snow_cover_3338</name><srs><number>3338</number></srs><zoomStart>0</zoomStart><zoomStop>7</zoomStop><format>image/png</format><type>reseed</type><threadCount>4</threadCount></seedRequest>'  "http://gs.mapventure.org:8080/geoserver/gwc/rest/seed/alaska_wildfires:snow_cover_3338.xml"
