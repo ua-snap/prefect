2bc4ea608b7ba14db12286534e9d1e8d3733dfcf
Bob Torgerson <rltorgerson@alaska.edu>
2024-11-20 16:39:38 -0900

    Activates Conda environment when a conda env is provided.

--- rasdaman/ingest_tasks.py
+++ rasdaman/ingest_tasks.py
@@ -146,6 +146,8 @@ def run_python_script(python_script, data_directory):
 def run_ingest(ingest_directory, ingest_file="ingest.json", conda_env=False):
     if conda_env:
         command = [
+            "source", 
+            "/opt/miniconda3/bin/activate;",
             "conda",
             "run",
             "-n",
@@ -166,4 +168,4 @@ def run_ingest(ingest_directory, ingest_file="ingest.json", conda_env=False):
         raise Exception(f"Error running the command. Error: {result.stderr}")
 
     print("Command Output:")
-    print(result.stdout)
+    print(result.stdout)""
