e59c0ffcc86510e764c267da803c045a8688dcf5
Bob Torgerson <rltorgerson@alaska.edu>
2024-12-06 15:38:42 -0900

    Updates GeoTIFF generation to use the correct coordinates and resolution.

--- hsia/seaice/seaice.py
+++ hsia/seaice/seaice.py
@@ -3,6 +3,7 @@ import numpy as np
 import rasterio
 from rasterio.transform import from_origin
 from rasterio.warp import calculate_default_transform, reproject, Resampling
+import os
 
 
 def netcdf_to_geotiff(input_netcdf, output_tiff):
@@ -25,7 +26,7 @@ def netcdf_to_geotiff(input_netcdf, output_tiff):
     # Pixel size is 25000 to match the 25 km grid
     pixel_size = 25000.0
     geotransform = (-3850000.0, pixel_size, 0.0, 5850000.0, 0.0, -pixel_size)
-    source_crs = "+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +a=6378273 +b=6356889.449 +units=m +no_defs"
+    source_crs = rasterio.CRS.from_string("EPSG:3411")
     target_crs = "EPSG:3572"
 
     with rasterio.MemoryFile() as memfile:
@@ -57,7 +58,7 @@ def netcdf_to_geotiff(input_netcdf, output_tiff):
                 }
             )
 
-            with rasterio.open(output_tiff, "w", **profile) as dst:
+            with rasterio.open("temp.tif", "w", **profile) as dst:
                 reproject(
                     source=rasterio.band(src, 1),
                     destination=rasterio.band(dst, 1),
@@ -68,4 +69,12 @@ def netcdf_to_geotiff(input_netcdf, output_tiff):
                     resampling=Resampling.nearest,
                 )
 
-    print(f"GeoTIFF {output_tiff} created successfully.")
+    # Use gdalwarp to overwrite the file with the correct coordinates
+    os.system(
+        "gdalwarp -overwrite -q -multi -t_srs EPSG:3572 -te_srs EPSG:3572 "
+        "-te -4862550.515 -4894840.007 4870398.248 4889334.803 "
+        "-tr 17075.348707767432643 -17075.348707767432643 "
+        f"'temp.tif' '{output_tiff}'"
+    )
+
+    print(f"GeoTIFF {output_tiff} created successfully")
