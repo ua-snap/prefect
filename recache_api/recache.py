80cecbd70d2c190c14b9c109050e87659479a03c
Bob Torgerson <rltorgerson@alaska.edu>
2025-02-18 12:59:06 -0900

    Updates name of function tasks.

--- recache_api/recache.py
+++ recache_api/recache.py
@@ -5,7 +5,7 @@ from luts import cached_urls
 
 @flow(log_prints=True)
 def recache_api(cached_url_list=cached_urls):
-    recache_tasks.run_recache(cached_url_list)
+    recache_tasks.recache_api(cached_url_list)
 
 
 if __name__ == "__main__":
