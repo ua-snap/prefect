7213bbbb4f6e80f35d2632b228ccd9fcf1aec673
SNAP Data User <cparr4@alaska.edu>
2025-07-25 13:49:06 -0800

    add sequential ingests

--- rasdaman/wrf_downscaled_era5_4km.py
+++ rasdaman/wrf_downscaled_era5_4km.py
@@ -19,7 +19,6 @@ def run_combine_netcdfs_script(ingest_directory: str, variable_name: str):
     # must activate conda
     command = [
             "bash",
-            "-i",
             "-c",
             f"source /opt/miniconda3/bin/activate rasdaman  && {' '.join(command)}",
     ]
@@ -29,8 +28,8 @@ def run_combine_netcdfs_script(ingest_directory: str, variable_name: str):
     # Using check=True will raise a CalledProcessError if the script fails (non-zero exit code)
     result = subprocess.run(
         command,
-        capture_output=True,
-        text=True,
+        #capture_output=True,
+        #text=True,
         check=True,
         cwd=ingest_directory
     )
@@ -59,7 +58,7 @@ def ingest_wrf_downscaled_era5_4km(
     # copy the data from the backed up source, and untar it and flatten it
     # then we need to combine the data into a single file
     # run the ingest command, these are all in the ingest directory with names like this:
-    # rainnc_sum_ingest.json  rh2_mean_ingest.json  seaice_max_ingest.json
+    # t2_min_ingest.json  rh2_mean_ingest.json  seaice_max_ingest.json
 
     for variable in era5_variables:
         dest_var_dir = f"{destination_directory}/{variable}"
@@ -78,14 +77,14 @@ if __name__ == "__main__":
     era5_variables = ["t2_mean", "t2_max", "t2_min", "rh2_mean", "rh2_max", "rh2_min", "wspd10_mean", "wspd10_max", "wdir10_mean", "rainnc_sum", "seaice_max"]
 
     ingest_wrf_downscaled_era5_4km.serve(
-        name="WRF Downscaled ERA5 4km Ingest(s)",
+        name="Rasdaman Coverage: ERA5 4km Daily Summaries (era5_4km_daily_$variable)",
         tags=["ARDAC", "ERA5"],
-        parameters={
-            "branch_name": "era5wrf",
-            "working_directory": "/opt/rasdaman/user_data/snapdata/",
-            "ingest_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/ardac/daily_wrf_downscaled_era5/",
-            "source_directory": "/workspace/Shared/Tech_Projects/daily_wrf_downscaled_era5_4km/",
-            "destination_directory": "/opt/rasdaman/user_data/snapdata/rasdaman-ingest/ardac/daily_wrf_downscaled_era5",
-            "era5_variables": era5_variables,
-        },
+        parameters = {
+           "branch_name": "era5wrf",
+           "working_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest",
+           "ingest_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest/rasdaman-ingest/ardac/daily_wrf_downscaled_era5/",
+           "source_directory": "/workspace/Shared/Tech_Projects/daily_wrf_downscaled_era5_4km/",
+           "destination_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest/rasdaman-ingest/ardac/daily_wrf_downscaled_era5",
+           "era5_variables": era5_variables,
+            }
     )
