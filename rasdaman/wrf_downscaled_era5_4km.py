7d6164959e77f199fbf0e0547ca876a3680b32b1
Bob Torgerson <rltorgerson@alaska.edu>
2025-08-04 13:53:00 -0800

    Adds task name for combining NetCDF files.

--- rasdaman/wrf_downscaled_era5_4km.py
+++ rasdaman/wrf_downscaled_era5_4km.py
@@ -8,7 +8,8 @@ import ingest_tasks
 
 # this flow can hit multiple ERA5 variables
 
-@task(log_prints=True)
+
+@task(name="Combine NetCDF Files", log_prints=True)
 def run_combine_netcdfs_script(ingest_directory: str, variable_name: str):
     """
     Runs the combine_netcdfs.py script for a given variable.
@@ -19,19 +20,15 @@ def run_combine_netcdfs_script(ingest_directory: str, variable_name: str):
     command = ["python", script_path, variable_name]
     # must activate conda
     command = [
-            "bash",
-            "-c",
-            f"source /opt/miniconda3/bin/activate rasdaman  && {' '.join(command)}",
+        "bash",
+        "-c",
+        f"source /opt/miniconda3/bin/activate rasdaman  && {' '.join(command)}",
     ]
 
     print(f"Executing command: {' '.join(command)}")
-    
-    result = subprocess.run(
-        command,
-        check=True,
-        cwd=ingest_directory
-    )
-    
+
+    result = subprocess.run(command, check=True, cwd=ingest_directory)
+
     print("NetCDF combination script execution successful.")
     print(f"STDOUT:\n{result.stdout}")
     if result.stderr:
@@ -64,12 +61,17 @@ def ingest_wrf_downscaled_era5_4km(
         var_ingest_recipe = f"{variable}_ingest.json"
 
         ingest_tasks.copy_data_from_nfs_mount(source_var_dir, destination_directory)
-        ingest_tasks.untar_file(f"{dest_var_dir}/{variable}_era5_4km_archive.tar.gz", ingest_directory, flatten=True, rename=variable)
+        ingest_tasks.untar_file(
+            f"{dest_var_dir}/{variable}_era5_4km_archive.tar.gz",
+            ingest_directory,
+            flatten=True,
+            rename=variable,
+        )
 
         run_combine_netcdfs_script(ingest_directory, variable)
 
         ingest_tasks.run_ingest(ingest_directory, var_ingest_recipe)
-        
+
         # for each ingest build a WMS link artifact
         wms_url = (
             "https://zeus.snap.uaf.edu/rasdaman/ows"
@@ -77,7 +79,7 @@ def ingest_wrf_downscaled_era5_4km(
             f"&layers=era5_4km_daily_{variable}"
             "&bbox=-1000000,600000,1000000,2500000"
             "&crs=EPSG:3338"
-            "&time=\"2001-11-04T00:00:00.000Z\""
+            '&time="2001-11-04T00:00:00.000Z"'
             "&width=1000&height=800"
             "&format=image/png&transparent=true&styles="
         )
@@ -86,25 +88,37 @@ def ingest_wrf_downscaled_era5_4km(
             wms_url,
             link_text=f"WMS preview: {variable}",
             description=(
-                "GetMap request for the "
-                f"`era5_4km_daily_{variable}` coverage"
-            )
+                "GetMap request for the " f"`era5_4km_daily_{variable}` coverage"
+            ),
         )
 
         time.sleep(10)
 
+
 if __name__ == "__main__":
-    era5_variables = ["t2_mean", "t2_max", "t2_min", "rh2_mean", "rh2_max", "rh2_min", "wspd10_mean", "wspd10_max", "wdir10_mean", "rainnc_sum", "seaice_max"]
+    era5_variables = [
+        "t2_mean",
+        "t2_max",
+        "t2_min",
+        "rh2_mean",
+        "rh2_max",
+        "rh2_min",
+        "wspd10_mean",
+        "wspd10_max",
+        "wdir10_mean",
+        "rainnc_sum",
+        "seaice_max",
+    ]
 
     ingest_wrf_downscaled_era5_4km.serve(
         name="Rasdaman Coverage: ERA5 4km Daily Summaries (era5_4km_daily_$variable)",
         tags=["ARDAC", "ERA5"],
-        parameters = {
-           "branch_name": "era5wrf",
-           "working_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest",
-           "ingest_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest/rasdaman-ingest/ardac/daily_wrf_downscaled_era5/",
-           "source_directory": "/workspace/Shared/Tech_Projects/daily_wrf_downscaled_era5_4km/",
-           "destination_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest/rasdaman-ingest/ardac/daily_wrf_downscaled_era5",
-           "era5_variables": era5_variables,
-            }
+        parameters={
+            "branch_name": "era5wrf",
+            "working_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest",
+            "ingest_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest/rasdaman-ingest/ardac/daily_wrf_downscaled_era5/",
+            "source_directory": "/workspace/Shared/Tech_Projects/daily_wrf_downscaled_era5_4km/",
+            "destination_directory": "/opt/rasdaman/user_data/snapdata/cp_dev_ras_ingest/rasdaman-ingest/ardac/daily_wrf_downscaled_era5",
+            "era5_variables": era5_variables,
+        },
     )
